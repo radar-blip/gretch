652f2e21bbe40784e12621010d7b0270d602542a
David <linuslagoo@yahoo.com>
2025-06-25 01:51:23 -0400

    more fixes

--- gretch.sh
+++ gretch.sh
@@ -10,23 +10,22 @@ printf "${bold}"
 
 who=$(id -u -n)
 where=$(hostname)
-printf '%s' "$who" "@" 
-printf "$where\n" 
-printf "$who" | tr "$who" '-' 
-printf "$where" | tr "$where" '-' | awk '{print $0"-"}'
+printf "%s" "$who" "@" "$where" 
+printf "$who" | tr "$who" '\n-' 
+printf "$where" | tr "$where" '-' | awk '{print $0"--"}'
 printf "\n"
 
 #for extracting OS/version
 . /etc/os-release
 
 #OS name (extracted from etc/os-release)
-printf "OS:%-9s" ; printf "$NAME\n"
+printf "OS:%-9s$NAME\n"
 
 #OS version/codename
-printf "Version:%-4s" ; printf "$VERSION\n"
+printf "Version:%-4s$VERSION\n"
 
 #desktop environment
-printf "DE:%-9s" ; printf "$XDG_CURRENT_DESKTOP\n" | tr -d 'X-'
+printf "DE:%-9s$XDG_CURRENT_DESKTOP\n" | tr -d 'X-'
 
 #kernel
 printf "Kernel:%-5s" ; uname -r
@@ -36,7 +35,7 @@ printf "Uptime:%-5s" ; uptime -p | cut -b 4-
 
 #shell/version
 (printf "Shell:%-6s" ; ps -o fname --no-headers $$
-printf $BASH_VERSION | cut -b 1-6) | tr '\n' ' ' ; printf "\n"
+printf "$BASH_VERSION" | cut -b 1-6) | tr '\n' ' ' ; printf "\n"
 
 
 #WM (window manager)
@@ -55,7 +54,7 @@ printf "Icons:%-4s" ; gtk-query-settings theme | grep 'gtk-icon-theme-name' | cu
 printf "Resolution:%-1s" ; xdpyinfo | awk '/dimensions/ {print $2}'
 
 #packages
-(printf "Packages:%-3s" ; dpkg --get-selections | wc --lines ; printf "(dpkg), "
+(printf "Packages:%-3s" ; dpkg --get-selections | wc -l ; printf "(dpkg), "
                           flatpak list | wc -l ; printf "(flatpak)") | tr '\n' ' ' ; printf "\n"
 
 
@@ -78,5 +77,8 @@ printf "Resolution:%-1s" ; xdpyinfo | awk '/dimensions/ {print $2}'
 
 #memory (in mebibytes)
 (printf "Memory:%-5s" ; free -m | grep -oP '\d+' | sed '1!d' ; printf "MiB(total), " 
-                        free -m | grep -oP '\d+' | sed '2!d' ; printf "MiB(used) ") | tr '\n' ' ' ; printf "\n"
-printf "${normal}\n"
+                        free -m | grep -oP '\d+' | sed '2!d' ; printf "MiB(used) ") | tr '\n' ' ' ; 
+
+printf "\n\n${normal}"
+
+
