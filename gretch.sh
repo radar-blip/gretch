86e715dd1578c8ffb69ce98fad0c6f8a9b211848
David <linuslagoo@yahoo.com>
2025-06-28 16:34:31 -0400

    added cpu speed back
    also corrected mistakes and fine tuned

--- gretch.sh
+++ gretch.sh
@@ -8,16 +8,16 @@ normal=$(tput sgr0)
 clear
 printf "${bold}"
 
-who=$(id -u -n)
+whom=$(id -u -n)
 where=$(hostname)
-printf '%s' "$who" "@" "$where" 
-printf '%s' "$who" | tr "$who" '\n-' 
+printf '%s' "$whom" "@" "$where" 
+printf '%s' "$whom" | tr "$whom" '\n-' 
 printf '%s' "$where" | tr "$where" '-' | awk '{print $0"--"}'
 printf "\n"
 
 
 #OS name/version
-printf "OS:%-9s" ; cat /etc/lsb-release | grep 'DISTRIB_DESCRIPTION' | cut -f 2 -d '"'
+printf "OS:%-9s" ; cat /etc/lsb-release | grep 'DISTRIB_DESCRIPTION' | cut -f2 -d '"'
 
 #desktop environment
 printf "DE:%-9s$XDG_CURRENT_DESKTOP\n" | tr -d 'X-'
@@ -26,21 +26,21 @@ printf "DE:%-9s$XDG_CURRENT_DESKTOP\n" | tr -d 'X-'
 printf "Kernel:%-5s" ; uname -r
 
 #uptime
-printf "Uptime:%-5s" ; uptime -p | cut -b 4-
+printf "Uptime:%-5s" ; uptime -p | cut -c 4-
 
 #shell/version
-(printf "Shell:%-6s" ; ps -o fname --no-headers $$
-printf "$BASH_VERSION" | cut -b 1-6) | tr '\n' ' ' ; printf "\n"
+printf "Shell:%-6s" ; ps -o fname --no-headers $$ | tr '\n' ' '
+printf "$BASH_VERSION" | cut -c 1-6 | tr '\n' ' ' ; printf "\n"
 
 
 #WM (window manager)
-printf "WM:%-8s" ; wmctrl -m | grep Name | cut -d: -f2
+printf "WM:%-8s" ; wmctrl -m | grep Name | cut -d ":" -f2
 
 #theme
-printf "Theme:%-4s" ; gtk-query-settings theme | grep 'gtk-theme-name' | cut -f 2 -d ":" | tr '\n"' ' ' ; printf "\b[GTK2/3]\n" 
+printf "Theme:%-4s" ; gtk-query-settings theme | grep 'gtk-theme-name' | cut -d ":" -f2 | tr '\n"' ' ' ; printf "\b[GTK2/3]\n" 
 
 #icons 
-printf "Icons:%-4s" ; gtk-query-settings theme | grep 'gtk-icon-theme-name' | cut -f 2 -d ":" | tr '\n"' ' ' ; printf "\b[GTK2/3]\n"
+printf "Icons:%-4s" ; gtk-query-settings theme | grep 'gtk-icon-theme-name' | cut -d ":" -f2 | tr '\n"' ' ' ; printf "\b[GTK2/3]\n"
 
 #desktop theme
 printf "Desktop:%-4s" ; gsettings get org.cinnamon.theme name | tr -d "''"
@@ -50,28 +50,27 @@ printf "Desktop:%-4s" ; gsettings get org.cinnamon.theme name | tr -d "''"
 printf "Resolution:%-1s" ; xdpyinfo | awk '/dimensions/ {print $2}'
 
 #packages
-if [ "flatpak list | wc -l < 1" ] 
-then
+if [ "flatpak list | wc -l < 1" ]; then
     printf "Packages:%-3s" ; dpkg --get-selections | wc -l | tr '\n' ' ' ; printf "(dpkg)" ; printf "\n"
-    
 else
-    (printf "Packages:%-3s" ; dpkg --get-selections | wc -l ; printf "(dpkg), "
-                              flatpak list | wc -l ; printf "(flatpak)") | tr '\n' ' ' ; printf "\n"
+    printf "Packages:%-3s" ; dpkg --get-selections | wc -l ; printf "(dpkg), "
+                             flatpak list | wc -l ; printf "(flatpak)" | tr '\n' ' ' ; printf "\n"
 fi
 
 
 #CPU 
-(printf "CPU:%-8s" ; lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1')
-
+printf "CPU:%-8s" ; lscpu | grep 'Model name' | cut -d ":" -f2 | awk '{$1=$1}1' | tr '\n' ' '
+                    #max speed - (if double output, comment out these lines)
+                    lscpu | grep 'max' | cut -d ":" -f2 | awk '{$1=$1}1' | awk '{printf "@ " substr($0, 1, length($0)-5)}' ; printf " Mhz" | tr '\n' ' ' ; printf "\n"
 
 #GPU 
-(printf "GPU:%-7s" ; lspci | grep 'VGA' | cut -d ":" -f3 | tr -d "[]")
+printf "GPU:%-7s" ; lspci | grep 'VGA' | cut -d ":" -f3 | tr -d "[]"
 
 
 #memory (in mebibytes)
 (printf "Memory:%-5s" ; free -m | grep -oP '\d+' | sed '1!d' ; printf "MiB(total), " 
                         free -m | grep -oP '\d+' | sed '2!d' ; printf "MiB(used) ") | tr '\n' ' '
 
-printf "\n\n${normal}"
+printf "${normal}\n\n"
 
 
