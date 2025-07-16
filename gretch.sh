17077b59342ae9fe1da185481069352bc2cbd23f
David <linuslagoo@yahoo.com>
2025-07-15 21:27:26 -0400

    updated DE
    to capitalize whichever desktop environment is in use

--- gretch.sh
+++ gretch.sh
@@ -28,6 +28,12 @@ printf "Version:%-4s" ; cat /etc/os-release | grep -m 1 'VERSION' | cut -d '"' -
 #desktop environment
 if [ $DESKTOP_SESSION == "cinnamon" ]; then
     printf "DE:%-9s$DESKTOP_SESSION\n" | tr 'c' 'C'
+elif [ $DESKTOP_SESSION == "mate" ]; then
+    printf "DE:%-9s$DESKTOP_SESSION\n" | tr 'm' 'M'
+elif [ $DESKTOP_SESSION == "xfce" ]; then
+    printf "DE:%-9s$DESKTOP_SESSION\n" | tr 'x' 'X'
+elif [ $DESKTOP_SESSION == "debian" ]; then
+    printf "DE:%-9s$DESKTOP_SESSION\n" | tr 'd' 'D'
 else
     printf "DE:%-9s$DESKTOP_SESSION\n"
 fi
@@ -79,13 +85,13 @@ else
 fi
 
 
-#CPU 
+#CPU
 if [ "AMD A4-6300" ]; then
     printf "CPU:%-8s" ; lscpu | grep 'Model name' | cut -d ":" -f 2 | awk '{$1=$1}1' | sed 's/w.*//' | tr '\n' ' '
-                        lscpu | grep 'max' | cut -d ":" -f 2 | awk '{$1=$1}1' | awk '{printf "\b@ " substr($0, 1, length($0)-5)}' ; printf " MHz\n" #short
+                        lscpu | grep 'max' | cut -d ":" -f 2 | awk '{$1=$1}1' | awk '{printf "\b@ " substr($0, 1, length($0)-5)}' | sed 's/$/ MHz\n/' #short
 else
     printf "CPU:%-8s" ; lscpu | grep 'Model name' | cut -d ":" -f 2 | awk '{$1=$1}1' | tr '\n' ' '
-                        lscpu | grep 'max' | cut -d ":" -f 2 | awk '{$1=$1}1' | awk '{printf "@ " substr($0, 1, length($0)-5)}' ; printf " MHz\n" #long output
+                        lscpu | grep 'max' | cut -d ":" -f 2 | awk '{$1=$1}1' | awk '{printf "@ " substr($0, 1, length($0)-5)}' | sed 's/$/ MHz\n/' #long output
 fi
 
 
@@ -102,8 +108,8 @@ fi
 
 
 #memory (in mebibytes)
-(printf "Memory:%-5s" ; free -m | grep -oP '\d+' | sed '2!d' ; printf "\bMiB / "
-                        free -m | grep -oP '\d+' | sed '1!d' ; printf "\bMiB ") | tr '\n' ' '
+(printf "Memory:%-5s" ; free -m | grep -oP '\d+' | sed '2!d' | sed 's/$/MiB \//'
+                        free -m | grep -oP '\d+' | sed '1!d' | sed 's/$/MiB /') | tr '\n' ' '
 
 
 printf "${norm}\n\n"
