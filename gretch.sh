c2a8e23582037b60e07c548950abcbd74f3ee3f3
radar-blip <39196767+radar-blip@users.noreply.github.com>
2025-10-03 20:44:07 +0000

    added on/off switch to show/hide architecture
    default: on

--- gretch.sh
+++ gretch.sh
@@ -11,6 +11,7 @@ yell=$(tput setaf 3)
 
 clear
 printf "${bold}"
+shopt -s nocasematch
 
 
 # User/Hostname
@@ -19,14 +20,30 @@ printf "$ghost" | tr '\n' '@'
 printf "\n${ghost//?/-}\n\n"
 
 
-# OS Name/Version
+# OS Name/Version 
 osname=$(grep -E '^NAME=|^VERSION=' /etc/os-release | cut -d '"' -f 2 | tr '\n' ' ')
-[[ -n "$osname" ]] && printf "%-11s %s\n" "OS:" "$osname"
+arch=$(uname -m)
+
+# on/off switch to show/hide architecture (ie x86_64)
+# on = show / off = hide
+arch_switch="on"
+
+case $arch_switch in
+    on)
+        [[ -n "$osname" ]] && printf "%-11s %s" "OS:" "$osname"
+        [[ -n "$arch" ]] && printf "%-11s %s\n" "$arch"
+        ;;
+    off)
+        [[ -n "$osname" ]] && printf "%-11s %s\n" "OS:" "$osname"
+        ;;
+    *)
+        printf "%-11s %s\n" "OS:" "$osname"
+        ;;
+esac
 
 
 # DE (desktop environment)
 de=$(printf $XDG_CURRENT_DESKTOP)
-shopt -s nocasematch
 
 case "$de" in
     gnome)
@@ -64,12 +81,12 @@ utime=$(uptime -p | cut -c 4-)
 
     
 # Shell
-sh=$(basename "$SHELL")
-sh_ver=$(basename "$BASH_VERSION" | cut -c 1-6)
+shell_name=$(basename "$SHELL")
+bash_ver=$(basename "$BASH_VERSION" | cut -c 1-6)
 
-case "$sh" in
+case "$shell_name" in
     bash)
-        printf "%-11s %s\n" "Shell:" "bash $sh_ver"
+        printf "%-11s %s\n" "Shell:" "bash $bash_ver"
         ;;
     sh)
         printf "%-11s %s\n" "Shell:" "sh"
@@ -93,7 +110,7 @@ case "$sh" in
         printf "%-11s %s\n" "Shell:" "tcsh"
         ;;
     *)
-        printf "%-11s %s\n" "Shell:" "$sh"
+        printf "%-11s %s\n" "Shell:" "$shell_name"
         ;;
 esac
 
@@ -141,6 +158,9 @@ case "$cpu_amd" in
     *amd* | *radeon* | *ryzen*)
         printf "%-11s %s %s\n" "CPU:" "$cpu_amd" "$max_ghz"
         ;;
+    *intel* | *core*)
+        printf "%-11s %s %s\n" "CPU:" "$cpu_def" "$max_ghz"
+        ;;
     *)
         printf "%-11s %s %s\n" "CPU:" "$cpu_def" "$max_ghz"
         ;;
@@ -149,10 +169,13 @@ esac
 
 # GPU
 gpu_def=$(lspci | awk '/VGA|3D|Display/ {print $0}' | cut -d ':' -f 3 | sed 's/^ //')
-gpu_amd=$(lspci | awk '/VGA|3D|Display/ {print $0}' | cut -d ':' -f 3 | sed 's/^ //' | awk -F'[][]' '{print $2,$4}')
+gpu_amd=$(lspci | awk '/VGA|3D|Display/ {print $0}' | cut -d ':' -f 3 | sed 's/^ //' | tr '/' ' ' | awk -F'[][]' '{print $2,$4}')
 case "$gpu_amd" in
     *amd* | *radeon*)
         printf "%-11s %s\n" "GPU:" "$gpu_amd"
+        ;;
+    *nvidia* | *geforce*)
+        printf "%-11s %s\n" "GPU:" "$gpu_def"
         ;;
     *)
         printf "%-11s %s\n" "GPU:" "$gpu_def"
