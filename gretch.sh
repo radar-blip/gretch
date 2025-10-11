24f380687c69f6bf6969f68c9adb340899d09283
radar-blip <39196767+radar-blip@users.noreply.github.com>
2025-10-11 03:39:11 +0000

    added terminal option and uptime switch
    added option, (and on/off switch) to show/hide terminal name
    
    also, on/off switch to show uptime in shorthand
    hours/minutes or hrs/mins

--- gretch.sh
+++ gretch.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 # gretch 
 # system info gathering script
-# for linux
-
+# for linux systems
 
 bold=$(tput bold)
 norm=$(tput sgr0)
 yell=$(tput setaf 3)
 
-
 clear
 printf "${bold}"
 shopt -s nocasematch
+
+
+# Switches
+
+# on/off switch to show/hide architecture (ie x86_64)
+# show = on / hide = off
+arch_switch="on"
+
+# on/off switch to show/hide terminal name
+# show = on / hide = off
+term_switch="on"  
+
+# on/off switch for uptime output
+# def = hours/minutes | min = hrs/mins
+# def = off / min = on
+uptime_switch="off"  
 
 
 # User/Hostname
@@ -20,14 +34,9 @@ printf "$ghost" | tr '\n' '@'
 printf "\n${ghost//?/-}\n\n"
 
 
-# OS Name/Version 
+# OS Name/Version + architecture
 osname=$(grep -E '^NAME=|^VERSION=' /etc/os-release | cut -d '"' -f 2 | tr '\n' ' ')
 arch=$(uname -m)
-
-# on/off switch to show/hide architecture (ie x86_64)
-# on = show / off = hide
-arch_switch="on"
-
 case $arch_switch in
     on)
         [[ -n "$osname" ]] && printf "%-11s %s" "OS:" "$osname"
@@ -44,7 +53,6 @@ esac
 
 # DE (desktop environment)
 de=$(printf $XDG_CURRENT_DESKTOP)
-
 case "$de" in
     gnome)
         printf "%-11s %s\n" "DE:" "Gnome"
@@ -76,14 +84,26 @@ kern=$(uname -r)
 
 
 # Uptime
-utime=$(uptime -p | cut -c 4-)
-[[ -n $utime ]] && printf "%-11s %s\n" "Uptime:" "$utime"
+uptime_def=$(uptime -p | cut -c 4-) 
+uptime_min=$(uptime -p | cut -c 4- | sed -e 's/hours/hrs/g' -e 's/minutes/mins/') #min
+
+case $uptime_switch in
+    on)
+        [[ -n $uptime_min ]] && printf "%-11s %s\n" "Uptime:" "$uptime_min"
+
+        ;;
+    off)
+        [[ -n $uptime_def ]] && printf "%-11s %s\n" "Uptime:" "$uptime_def"
+        ;;
+    *)
+        [[ -n $uptime_def ]] && printf "%-11s %s\n" "Uptime:" "$uptime_def"
+        ;;
+esac
 
     
 # Shell
 shell_name=$(basename "$SHELL")
 bash_ver=$(basename "$BASH_VERSION" | cut -c 1-6)
-
 case "$shell_name" in
     bash)
         printf "%-11s %s\n" "Shell:" "bash $bash_ver"
@@ -137,6 +157,25 @@ dtheme=$(gsettings get org.cinnamon.theme name 2>/dev/null | tr -d "''")
 [[ -n "$dtheme" ]] && printf "%-11s %s\n" "Desktop:" "$dtheme"
 
 
+# Terminal
+term=$(ps -o 'cmd=' -p $(ps -o 'ppid=' -p $(ps -o 'ppid=' -p $$ 2>/dev/null)))
+case $term_switch in
+    on)
+        if [[ $osname == *"mint"* ]]; then
+            printf "%-11s%s\n" "Terminal:" "${term%-*}" | awk -F '/' '{print $1, $4}'
+        else
+            printf "%-11s %s\n" "Terminal:" "${term%%.*}"
+        fi
+        ;;
+    off)
+        [[ -n "$term" ]] && printf ""
+        ;;
+    *)
+        printf ""
+        ;;
+esac
+
+
 # Resolution
 res=$(xdpyinfo | awk '/dimensions/ {print $2}')
 [[ -n $"res" ]] && printf "%-11s %s\n" "Resolution:" "$res"
@@ -151,28 +190,28 @@ flatpaks=$(flatpak list | wc -l)
 
 
 # CPU
+cpu_mod=$(lscpu | awk -F': *' '/Model name/{printf $2}' | cut -d ' ' -f 1,2,3)
 cpu_def=$(lscpu | awk -F': *' '/Model name/{printf $2}')
-cpu_amd=$(lscpu | awk -F': *' '/Model name/{printf $2}' | cut -d ' ' -f 1,2,3)
 max_ghz=$(lscpu | awk -F': *' '/CPU max MHz/{printf "@ " ($2/1000) " GHz"}')
-case "$cpu_amd" in
+case "$cpu_mod" in
     *amd* | *radeon* | *ryzen*)
-        printf "%-11s %s %s\n" "CPU:" "$cpu_amd" "$max_ghz"
+        printf "%-11s %s %s\n" "CPU:" "$cpu_mod" "$max_ghz"
         ;;
     *intel* | *core*)
-        printf "%-11s %s %s\n" "CPU:" "$cpu_def" "$max_ghz"
+        printf "%-11s %s %s\n" "CPU:" "$cpu_def" 
         ;;
     *)
-        printf "%-11s %s %s\n" "CPU:" "$cpu_def" "$max_ghz"
+        printf "%-11s %s %s\n" "CPU:" "$cpu_def" 
         ;;
 esac
 
 
 # GPU
+gpu_mod=$(lspci | awk '/VGA|3D|Display/ {print $0}' | cut -d ':' -f 3 | awk -F'[][]' '{print $2,$4}' | tr '/' ' ')
 gpu_def=$(lspci | awk '/VGA|3D|Display/ {print $0}' | cut -d ':' -f 3 | sed 's/^ //')
-gpu_amd=$(lspci | awk '/VGA|3D|Display/ {print $0}' | cut -d ':' -f 3 | sed 's/^ //' | tr '/' ' ' | awk -F'[][]' '{print $2,$4}')
-case "$gpu_amd" in
+case "$gpu_mod" in
     *amd* | *radeon*)
-        printf "%-11s %s\n" "GPU:" "$gpu_amd"
+        printf "%-11s %s\n" "GPU:" "$gpu_mod"
         ;;
     *nvidia* | *geforce*)
         printf "%-11s %s\n" "GPU:" "$gpu_def"
